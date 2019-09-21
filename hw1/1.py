69b9b1f7550036b1784f49b4c7df703950a29ca5
ronidrell <54766900+ronidrell@users.noreply.github.com>
2019-09-21 14:19:35 +0900

    Update 1.py

--- hw1/1.py
+++ hw1/1.py
@@ -1,9 +1,9 @@
 def get_digit_sum(n):
     n = abs(n)
-    s=0
+    s = 0
     while n != 0:
-        s = s + n%10
-        n = n//10
+        s = s + n % 10
+        n = n // 10
     return s
 
 if __name__ == "__main__":
