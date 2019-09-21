ff7955fa93493d43fc340bb77138985592d0264c
ronidrell <54766900+ronidrell@users.noreply.github.com>
2019-09-21 14:21:03 +0900

    Update 2.py

--- hw1/2.py
+++ hw1/2.py
@@ -1,15 +1,15 @@
 def get_digit_sum(n):
     n = abs(n)
-    s=0
+    s = 0
     while n != 0:
-        s = s + n%10
-        n = n//10
+        s = s + n % 10
+        n = n // 10
     return s
 
 
 def is_beaty(n):
     sum = get_digit_sum(n)
-    if n%sum == 0:
+    if n % sum == 0:
         return True
     else:
         return False
