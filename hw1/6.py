bf4534fac00e247b9bf234477c87acd7b931e333
ronidrell <54766900+ronidrell@users.noreply.github.com>
2019-09-22 21:23:55 +0900

    Update 6.py

--- hw1/6.py
+++ hw1/6.py
@@ -3,7 +3,9 @@ def get_largest_perimeter(L):
     for i in range(0, len(L) - 2):
         for j in range(i + 1, len(L) - 1):
             for k in range(j + 1, len(L)):
-                if L[i] + L[j] > L[k] and L[i] + L[k] > L[j] and L[j] + L[k] > L[i]:
+                if L[i] + L[j] > L[k] and 
+                        L[i] + L[k] > L[j] and 
+                        L[j] + L[k] > L[i]:
                     if L[i] + L[j] + L[k] > maxPer:
                         maxPer = L[i] + L[j] + L[k]
     if maxPer == 0:
@@ -13,6 +15,6 @@ def get_largest_perimeter(L):
 
 if __name__ == "__main__":
     print(get_largest_perimeter([
-        12, 5, 3, 
-        7, 1, 8
-    ]))
+                        12, 5, 3, 
+                        7, 1, 8
+                                ]))
