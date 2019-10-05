b64a9af00d941be8cb11ace3fde3d5ea73aceba5
ronidrell <54766900+ronidrell@users.noreply.github.com>
2019-10-05 14:51:44 +0900

    Update 8.py

--- hw2/8.py
+++ hw2/8.py
@@ -5,11 +5,17 @@ def get_partition(S):
     i = 0
     while S != "":
         answer.append("")
-        sub_letters = list(set(S[:S.rfind(S[0]) + 1]))
-        begin = min([S.find(letter) for letter in sub_letters])
-        end = max([S.rfind(letter) for letter in sub_letters])
-        answer[i] += S[begin:end + 1]
-        S = S[end + 1:]
+        s = False
+        sub_letters = set(S[:S.rfind(S[0]) + 1])
+        while s == False:
+            begin = min([S.find(letter) for letter in list(sub_letters)])
+            end = max([S.rfind(letter) for letter in list(sub_letters)])
+            answer[i] += S[begin:end + 1]
+            S = S[end + 1:]
+            if sub_letters == set(answer[i]):
+                s = True
+            else:
+                sub_letters = set(answer[i])
         i += 1
     return answer
 
